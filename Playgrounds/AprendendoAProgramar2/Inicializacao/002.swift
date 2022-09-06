84eda7f38f26d95b15218950c83c545e53e6f205
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-06 15:37:21 -0300

    Update 002.swift

--- Playgrounds/AprendendoAProgramar2/Inicializacao/002.swift
+++ Playgrounds/AprendendoAProgramar2/Inicializacao/002.swift
@@ -1,1 +1,41 @@
-.
+let expert = Expert()
+
+var gems = 0
+
+func andar(){
+    expert.moveForward()
+    expert.moveForward()
+    if expert.isOnGem{
+        expert.collectGem()
+        gems+=1
+        expert.turnLockDown()
+    }
+}
+func centro(){
+    expert.turnLeft()
+    expert.turnLeft()
+    expert.moveForward()
+    expert.moveForward()
+    expert.turnRight()
+    ()
+}
+
+func mudarLado(){
+    expert.moveForward()
+    expert.moveForward()
+    expert.moveForward()
+    expert.turnRight()
+    ()
+}
+
+while true{
+    for i in 1...3{
+    andar()
+    centro()
+        
+    }
+    if gems==6{
+        break
+    }
+    mudarLado()
+}
