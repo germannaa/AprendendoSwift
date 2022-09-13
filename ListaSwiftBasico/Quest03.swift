cb528119e2c2b54fa74405ffe79045c2ad632aec
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-13 14:30:36 -0300

    Create Quest03.swift

---
+++ ListaSwiftBasico/Quest03.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let  rating1:Double = 876
+let  rating2:Double = 532
+let  rating3:Double = 2917
+
+let averageRating:Double = (rating1+rating2+rating3)/3
+
+print("A média dos valores \(rating1), \(rating2) e \(rating3) é de \(averageRating).")
+
+
+//Foi usado a tipagem Double, pois a media poderia dar ou não um valor fracionado. 
