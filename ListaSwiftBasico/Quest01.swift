576ef96e0d42446065d894e6042a77a3723930aa
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-13 14:11:42 -0300

    Create Quest01.swift

---
+++ ListaSwiftBasico/Quest01.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let myAge = 35
+var dogs = 3
+
+print("Olá! Me chamo Germanna, tenho \(myAge) anos e possuo \(dogs) cachorros.")
+
+dogs+=1
+
+print("Comprei mais um filhote, e atualmente possuo \(dogs).")
