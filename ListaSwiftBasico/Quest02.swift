0b1bf9737b4646f68f5e57fef5cdacc192fd79ae
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-13 14:22:48 -0300

    Create Quest02.swift

---
+++ ListaSwiftBasico/Quest02.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var age = 16
+
+print(age)
+
+age = 30
+
+print(age)
+
+//Foi usado 'var' no lugar de let, pois o valor iria mudar, e sendo ssim, nao poderiamos considerar uma constante, mas sim uma variavel.
