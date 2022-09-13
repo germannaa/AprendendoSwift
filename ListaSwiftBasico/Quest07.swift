24b18285a65649a4aac0c7ef5e67291c8fa09484
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-13 16:08:33 -0300

    Create Quest07.swift

---
+++ ListaSwiftBasico/Quest07.swift
@@ -0,0 +1,8 @@
+let character1: Character = "Dog"
+let character2: Character = "🐶"
+let string1: String = "Dog"
+let string2: String = "🐶"
+
+
+// A constante character 1 vai dar erro, pois ela nao admite string.
+//Ja as strings, aceitam qualquer coisa
