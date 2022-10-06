697fda8fbfb8b2531688c22c3b53b5ffe9193af7
Germanna Rebouças <ger_manna@hotmail.com>
2022-10-06 14:28:24 -0300

    Create 001-HelloWorld.swift

---
+++ SwiftUI/001-HelloWorld.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Germanna Reboucas on 06/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!!")
+                .font(.largeTitle)
+                .foregroundColor(Color.purple)
+            Text("My first Hello World - Swift UI")
+                
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
