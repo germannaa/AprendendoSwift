0df5d6d8536053be36c39930ee2d0e376e72b822
Germanna Rebouças <ger_manna@hotmail.com>
2022-10-07 15:22:46 -0300

    Add files via upload

---
+++ SwiftUI/First/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by aluno on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
